334a3d091cf98163d446336c563cc95134246d9f
rahimkomac <rahimkomac@teknasyon.com>
2020-07-27 16:27:13 +0300

    likers model mapper added fields

--- src/InstagramScraper/Model/Like.php
+++ src/InstagramScraper/Model/Like.php
@@ -15,6 +15,16 @@ class Like extends AbstractModel
      */
     protected $username;
 
+    protected $full_name;
+
+    protected $profile_pic_url;
+
+    protected $is_verified;
+
+    protected $followed_by_viewer;
+
+    protected $requested_by_viewer;
+
     /**
      * @return mixed
      */
@@ -32,6 +42,37 @@ class Like extends AbstractModel
     }
 
     /**
+     * @return mixed
+     */
+    public function getFullName()
+    {
+        return $this->full_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProfilePicUrl()
+    {
+        return $this->profile_pic_url;
+    }
+
+    public function isVerified()
+    {
+        return $this->is_verified;
+    }
+
+    public function getFollowedByViewer()
+    {
+        return $this->followed_by_viewer;
+    }
+
+    public function getRequestedByViewer()
+    {
+        return $this->requested_by_viewer;
+    }
+
+    /**
      * @param $value
      * @param $prop
      */
@@ -43,6 +84,21 @@ class Like extends AbstractModel
                 break;
             case 'username':
                 $this->username = $value;
+                break;
+            case 'full_name':
+                $this->full_name = $value;
+                break;
+            case 'profile_pic_url':
+                $this->profile_pic_url = $value;
+                break;
+            case 'is_verified':
+                $this->is_verified = $value;
+                break;
+            case 'followed_by_viewer':
+                $this->followed_by_viewer = $value;
+                break;
+            case 'requested_by_viewer':
+                $this->requested_by_viewer = $value;
                 break;
         }
     }
